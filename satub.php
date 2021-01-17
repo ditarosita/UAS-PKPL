37f8be3e4f8bfc2b39edf6f66a99c68dd457284c
ditarosita <71695544+ditarosita@users.noreply.github.com>
2021-01-17 14:59:04 +0700

    Add files via upload
    unrefactored polymorphism

---
+++ satub.php
@@ -0,0 +1,36 @@
+<?php
+class Tabung{
+	public $phi;
+	public function __construct($phi=3.14){
+        $this->phi = $phi;
+	}
+	public function luasSelimutTabung($jarijari, $tinggi){
+		$dua = 2;
+		echo "Luas selimut tabung :". $dua*$this->phi*$jarijari*$tinggi;
+	}
+	public function luasTabung($jarijari, $tinggi){
+		$dua = 2;
+		echo "Luas tabung :". $dua*$this->phi*$jarijari*($jarijari+$tinggi);
+	}
+	public function kelilingAlasTabung ($jarijari){
+		$dua = 2;
+		echo "Keliling alas tabung :". $dua*$this->phi*$jarijari;
+	}
+	public function tendensiSentralUntuk($tendensiSentral, $jarijari, $tinggi){
+		switch ($tendensiSentral){
+			case 'luasSelimut':
+				$this->luasSelimutTabung($jarijari, $tinggi);
+				break;
+			case 'luas':
+				$this->luasTabung($jarijari, $tinggi);
+				break
+			case 'keliling':
+				$this->kelilingAlasTabung($jarijari);
+				break
+
+		}
+	}
+
+$jarijari = 14;
+$hitungTabung->tendensiSentralUntuk('luas', $jarijari, $tinggi)
+?>
