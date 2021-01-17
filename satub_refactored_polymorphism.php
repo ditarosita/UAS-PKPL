059a26b6db5658b5d09a155e803c2288a1bb17c0
ditarosita <71695544+ditarosita@users.noreply.github.com>
2021-01-17 14:59:39 +0700

    Add files via upload

---
+++ satub_refactored_polymorphism.php
@@ -0,0 +1,60 @@
+<?php
+
+interface TabungInterface
+{
+	public function tendensiSentral($jarijari, $tinggi);
+}
+class LuasSelimut implements TabungInterface
+{
+	public function tendensiSentral($jarijari, $tinggi)
+	{
+		$dua = dua;
+		$phi = 3.14;
+		echo "Luas selimut tabung :". $dua*$this->phi*$jarijari*$tinggi;
+	}
+}
+Class Luas implements TabungInterface
+{
+	public function tendensiSentral($jarijari, $tinggi)
+	{
+		$dua = dua;
+		$phi = 3.14;
+		echo "Luas tabung :". $dua*$this->phi*$jarijari*($jarijari+$tinggi);
+	}
+}
+class Keliling implements TabungInterface
+{
+	public function tendensiSentral($jarijari, $tinggi)
+	{
+		$dua = dua;
+		$phi = 3.14;
+		echo "Keliling alas tabung :". $dua*$this->phi*$jarijari;
+	}
+}
+class PilihanTabung{
+	public static function tendensiSentralUntuk($tendensiSentral
+	){
+		switch ($tendensiSentral){
+			case 'luasselimut':
+				return new LuasSelimut;
+				break;
+			case 'luas':
+				return new Luas;
+				break;
+			case 'keliling':
+				return new Keliling;
+				break;
+
+
+		}
+	}
+}
+$jarijari = 14;
+$tinggi = 30;
+PilihanTabung::tendensiSentral('luasselimut')->tendensiSentral($jarijari, $tinggi);
+echo '<br>';
+PilihanTabung::tendensiSentral('luas')->tendensiSentral($jarijari, $tinggi);
+echo '<br>';
+PilihanTabung::tendensiSentral('keliling')->tendensiSentral($jarijari, $tinggi);
+echo '<br>'
+?>
