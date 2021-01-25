a33fb9b3be5a608fc3d5c8f4aef1247b3a58c845
ditarosita <71695544+ditarosita@users.noreply.github.com>
2021-01-25 12:49:08 +0700

    Update dua_a_TestFungsionalRouteDanView.php

--- dua_a_TestFungsionalRouteDanView.php
+++ dua_a_TestFungsionalRouteDanView.php
@@ -18,7 +18,7 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(300);
     }
 
     // Function untuk testing route dan view login dosen
